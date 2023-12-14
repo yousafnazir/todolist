c18894341c943895d810e5ad36205bda1f5e6c02
Yousaf Nazir <yousaf.nazir2023@gmail.com>
2023-12-15 01:36:01 +0500

    last commit

--- script.js
+++ script.js
@@ -15,13 +15,22 @@ function addTask (){
     li.appendChild(span)   
 
   } 
+  inputbox.value = "";
+  saveData()
 }
 
 listcontainer.addEventListener("click", function(e){
   if(e.target.tagName === "LI"){
     e.target.classList.toggle("checked");
+    saveData()
   }
   else if(e.target.tagName === "SPAN"){
     e.target.parentElement.remove();
+    saveData()
   }
 }, false);
+
+function saveData(){
+  localStorage.setItem("data", listcontainer.innerHTML);
+
+}
